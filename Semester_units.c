3a53e7774e62b73ad3add12643ca5bb0ff1e2f55
alekokim <alekokim95@gmail.com>
2025-10-03 09:26:32 +0300

    Update Semester_units.c

--- Semester_units.c
+++ Semester_units.c
@@ -1,4 +1,4 @@
-#// simple C program(my units)single line comment
+// simple C program(my units)single line comment
 /*
 Name:Alex
 Reg:CT100/G/26186/25
@@ -7,9 +7,7 @@ Description:My units this semester
 #include <stdio.h>
 
 int main() {
-    printf("=====================================\n");
     printf("  My units this semester \n");
-    printf("=====================================\n\n");
 
     printf("1. Introduction to Computer Application\n");
     printf("2. Introduction to Programming and Algorithms\n");
@@ -19,10 +17,6 @@ int main() {
     printf("6. Computer system and Organisation \n");
     printf("7. Analogue Electronics \n");
     printf("8.Essential of Accounting \n");
-    
-    printf("\n=====================================\n");
-    printf("   End of Units List\n");
-    printf("=====================================\n");
 
     return 0;
 }
